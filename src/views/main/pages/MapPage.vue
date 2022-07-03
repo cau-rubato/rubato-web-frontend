3a193a5f77c56c36c09c6140f02fe2f3ff0953fd
SeungYunBaek <ryusim@cau.ac.kr>
2022-07-04 02:25:59 +0900

    [fix/mappage] Fix height of map
    src/views/main/pages/MapPage.vue

--- src/views/main/pages/MapPage.vue
+++ src/views/main/pages/MapPage.vue
@@ -3,8 +3,8 @@
     <div class="container page px-5">
       <h2 class="page-title main-color">오시는 길</h2>
       <h2 class="mt-5">서울특별시 동작구 흑석로 84</h2>
-      <h2 class="display-4 page-head">중앙대학교 서울캠퍼스 학생회관 607호</h2>
-      <iframe class="mt-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.1631557682003!2d126.9561179155765!3d37.504069879809585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca1d94bd4a94b%3A0x9f778bbcbcaf0387!2z7ISc7Jq47Yq567OE7IucIO2dkeyEneuPmSDspJHslZnrjIDtlZnqtZAg7ZWZ7IOd7ZqM6rSA!5e0!3m2!1sko!2skr!4v1654434449824!5m2!1sko!2skr" width="100%" height="600px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <h2 class="display-5 page-head word-keep-all">중앙대학교 서울캠퍼스 학생회관 607호</h2>
+      <iframe class="mt-3 map-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.1631557682003!2d126.9561179155765!3d37.504069879809585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca1d94bd4a94b%3A0x9f778bbcbcaf0387!2z7ISc7Jq47Yq567OE7IucIO2dkeyEneuPmSDspJHslZnrjIDtlZnqtZAg7ZWZ7IOd7ZqM6rSA!5e0!3m2!1sko!2skr!4v1654434449824!5m2!1sko!2skr" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
   </div>
@@ -17,5 +17,15 @@ export default {
 </script>
 
 <style scoped>
+.map-frame {
+  height: 300px;
+}
+
+@media (min-width: 576px) {
+  .map-frame {
+    height: 600px;
+  }
+}
+
 
 </style>
