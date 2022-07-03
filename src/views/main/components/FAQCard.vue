912839cd6abc4db27c95965992e063486adabfa4
SeungYunBaek <ryusim@cau.ac.kr>
2022-07-04 02:13:42 +0900

    [feature/faqpage](#35) Develop FAQPage, FAQCard, FAQDetail, Tests
    src/assets/styles
    src/store
    src/views/main/components
    src/views/main/pages
    tests/unit/views/main/components
    tests/unit/views/main/pages

---
+++ src/views/main/components/FAQCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="faq-card mb-1" @click="$emit('selected', this.faq.id)">
+    <div class="row">
+      <h4 class="quest"><div class="qicon main-color">Q.</div>{{ faq.question }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FAQCard",
+  props: {
+    faq: Object
+  }
+}
+</script>
+
+<style scoped>
+.faq-card {
+  width: 100%;
+  padding-bottom: 1rem;
+  padding-top: 1.2rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  border-radius: 8px;
+}
+
+.faq-card:hover {
+  cursor: pointer;
+  background-color: #E8E9Ea;
+}
+
+.quest {
+  font-weight: 400;
+  line-height: 150%;
+  text-align: left;
+  word-break: keep-all;
+}
+
+.qicon {
+  display: inline;
+  margin-right: 0.5rem;
+}
+
+</style>
---
+++ src/views/main/components/FAQDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h6 class="mb-5 return-list" @click="$emit('return')">
+      <i class="bi bi-chevron-left"
+         style="margin-right: 10px;"></i>질문 목록으로 돌아가기</h6>
+
+    <h1 class="word-keep-all">{{ $store.state.faqs[faqId].question }}</h1>
+    <p class="mt-5 lead">{{ $store.state.faqs[faqId].answer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FAQDetail",
+  props: {
+    faqId: Number
+  }
+}
+</script>
+
+<style scoped>
+.return-list:hover {
+  cursor: pointer;
+  color: #277DA1;
+}
+</style>
